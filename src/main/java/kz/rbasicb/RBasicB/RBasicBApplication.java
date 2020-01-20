987337a6e27bbf4d7ce129200f9bcff3d94217ab
maraun <mukhtargaliyev.rauan@gmail.com>
2020-01-20 12:21:50 +0600

    Spring initializr

---
+++ src/main/java/kz/rbasicb/RBasicB/RBasicBApplication.java
@@ -0,0 +1,13 @@
+package kz.rbasicb.RBasicB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RBasicBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RBasicBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/rbasicb/RBasicB/RBasicBApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.rbasicb.RBasicB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RBasicBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
