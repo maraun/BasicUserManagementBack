391f0ca0c9e683cb34766d047f70328218c32260
maraun <mukhtargaliyev.rauan@gmail.com>
2020-01-27 12:59:03 +0600

    Created Request, Response message class

---
+++ src/main/java/kz/rbasicb/RBasicB/models/messages/response/JwtResponse.java
@@ -0,0 +1,56 @@
+package kz.rbasicb.RBasicB.models.messages.response;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
+public class JwtResponse {
+    private String token;
+    private String type = "Bearer";
+    private String username;
+    private String firstname;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public JwtResponse(String accessToken, String username, String firstname, Collection<? extends GrantedAuthority> authorities) {
+        this.token = accessToken;
+        this.username = username;
+        this.authorities = authorities;
+        this.firstname = firstname;
+    }
+
+    public String getAccessToken() {
+        return token;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.token = accessToken;
+    }
+
+    public String getTokenType() {
+        return type;
+    }
+
+    public void setTokenType(String tokenType) {
+        this.type = tokenType;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String username) {
+        this.firstname = firstname;
+    }
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+}
