f39cb0cfb7646823303780e3424d0d376843f72d
maraun <mukhtargaliyev.rauan@gmail.com>
2020-01-28 18:05:51 +0600

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,61 @@
+INSERT INTO rbasic.additional (id, created_at, deleted_at, updated_at, information) VALUES (1, '2020-01-28 08:32:06', null, '2020-01-28 08:32:12', 'Additional information for id1');
+INSERT INTO rbasic.additional (id, created_at, deleted_at, updated_at, information) VALUES (2, '2020-01-28 08:34:10', null, '2020-01-28 08:34:11', 'Additional information for id2');
+
+INSERT INTO rbasic.citizenship (id, created_at, deleted_at, updated_at, name) VALUES (1, '2020-01-28 08:32:40', null, '2020-01-28 08:32:42', 'KAZAKHSTAN');
+INSERT INTO rbasic.citizenship (id, created_at, deleted_at, updated_at, name) VALUES (2, '2020-01-28 08:32:55', null, '2020-01-28 08:32:59', 'RUSSIANFEDERATION');
+INSERT INTO rbasic.citizenship (id, created_at, deleted_at, updated_at, name) VALUES (3, '2020-01-28 08:33:53', null, '2020-01-28 08:33:55', 'USA');
+
+INSERT INTO rbasic.contacts (id, created_at, deleted_at, updated_at, email, homephone, mobilephone, workphone) VALUES (1, '2020-01-28 08:53:14', null, '2020-01-28 08:53:16', 'email@mail.em', '87222321456', '87775632147', '87222369852');
+INSERT INTO rbasic.contacts (id, created_at, deleted_at, updated_at, email, homephone, mobilephone, workphone) VALUES (2, '2020-01-28 08:53:51', null, '2020-01-28 08:53:53', 'po4ta@mail.ru', '87222369852', '87074567896', '87222123456');
+
+INSERT INTO rbasic.documents (id, created_at, deleted_at, updated_at, identity_number, issued_date, issued_place, serial_number, type, validity_date) VALUES (1, '2020-01-28 08:37:52', null, '2020-01-28 08:37:53', '01236547', '2012-01-28 08:38:06', 'MVD RK', '99123587', 'IDENTITY_CARD', '2025-01-28 08:38:40');
+INSERT INTO rbasic.documents (id, created_at, deleted_at, updated_at, identity_number, issued_date, issued_place, serial_number, type, validity_date) VALUES (2, '2020-01-28 08:37:52', null, '2020-01-28 08:37:53', '36510599', '2012-01-28 08:38:06', 'MJ RK', '65147536', 'PASSPORT', '2025-01-28 08:38:40');
+INSERT INTO rbasic.documents (id, created_at, deleted_at, updated_at, identity_number, issued_date, issued_place, serial_number, type, validity_date) VALUES (3, '2020-01-28 08:37:52', null, '2020-01-28 08:37:53', '98489412', '2012-01-28 08:38:06', 'MVD RK', '89742354', 'BIRTH_CERTIFICATE', '2025-01-28 08:38:40');
+INSERT INTO rbasic.documents (id, created_at, deleted_at, updated_at, identity_number, issued_date, issued_place, serial_number, type, validity_date) VALUES (4, '2020-01-28 08:37:52', null, '2020-01-28 08:37:53', '89451651', '2012-01-28 08:38:06', 'MVD RK', '18565123', 'RESIDENCE_PERMIT', '2025-01-28 08:38:40');
+INSERT INTO rbasic.documents (id, created_at, deleted_at, updated_at, identity_number, issued_date, issued_place, serial_number, type, validity_date) VALUES (5, '2020-01-28 08:37:52', null, '2020-01-28 08:37:53', '02020561', '2012-01-28 08:38:06', 'MJ RK', '01546166', 'OTHER', '2025-01-28 08:38:40');
+
+INSERT INTO rbasic.genders (id, created_at, deleted_at, updated_at, name) VALUES (1, '2020-01-28 08:42:48', null, '2020-01-28 08:42:43', 'MALE');
+INSERT INTO rbasic.genders (id, created_at, deleted_at, updated_at, name) VALUES (2, '2020-01-28 08:42:56', null, '2020-01-28 08:42:59', 'FEMALE');
+
+INSERT INTO rbasic.marital_status (id, created_at, deleted_at, updated_at, name) VALUES (1, '2020-01-28 08:44:22', null, '2020-01-28 08:44:24', 'SINGLE');
+INSERT INTO rbasic.marital_status (id, created_at, deleted_at, updated_at, name) VALUES (2, '2020-01-28 08:44:31', null, '2020-01-28 08:44:32', 'MARRIED');
+
+INSERT INTO rbasic.nationalities (id, created_at, deleted_at, updated_at, name) VALUES (1, '2020-01-28 08:55:18', null, '2020-01-28 08:55:19', 'KAZAKH');
+INSERT INTO rbasic.nationalities (id, created_at, deleted_at, updated_at, name) VALUES (2, '2020-01-28 08:55:44', null, '2020-01-28 08:55:43', 'RUSSIAN');
+INSERT INTO rbasic.nationalities (id, created_at, deleted_at, updated_at, name) VALUES (3, '2020-01-28 08:55:53', null, '2020-01-28 08:55:54', 'AMERICAN');
+
+INSERT INTO rbasic.positions (id, created_at, deleted_at, updated_at, finishtime, organization, position, starttime) VALUES (1, '2020-01-28 08:56:57', null, '2020-01-28 08:56:58', '2020-01-28 08:57:01', 'SMU', 'Teacher', '2014-01-28 08:57:17');
+INSERT INTO rbasic.positions (id, created_at, deleted_at, updated_at, finishtime, organization, position, starttime) VALUES (2, '2020-01-28 08:57:36', null, '2020-01-28 08:57:37', '2019-01-28 08:57:39', 'KazMinerals', 'Director', '2013-01-28 08:58:04');
+INSERT INTO rbasic.positions (id, created_at, deleted_at, updated_at, finishtime, organization, position, starttime) VALUES (3, '2020-01-28 08:58:15', null, '2020-01-28 08:58:16', '2017-01-28 08:58:18', 'KazMunaiGaz', 'Chief Manager', '2004-01-28 08:58:57');
+INSERT INTO rbasic.positions (id, created_at, deleted_at, updated_at, finishtime, organization, position, starttime) VALUES (4, '2020-01-28 08:59:06', null, '2020-01-28 08:59:08', '2019-01-28 08:59:09', 'Gazprom', 'Manager', '2011-01-28 08:59:26');
+INSERT INTO rbasic.positions (id, created_at, deleted_at, updated_at, finishtime, organization, position, starttime) VALUES (5, '2020-01-28 08:59:34', null, '2020-01-28 08:59:35', '2017-01-28 08:59:37', 'BRK', 'Head of office', '2009-01-28 09:00:11');
+
+INSERT INTO rbasic.profile (id, created_at, deleted_at, updated_at, birthdate, firstname, iin, lastname, living_place, middlename, photo_name, previous_lastname, registration_place, citizenship_id, gender_id, marital_status_id, nationality_id) VALUES (1, '2020-01-28 09:01:04', null, '2020-01-28 09:01:05', '1987-01-28 09:01:07', 'Henry', '870128123456', 'Joshua', 'Abaya 103', 'Sun', 'joshua45', null, 'Kabanbai batyr 555', 3, 1, 1, 3);
+INSERT INTO rbasic.profile (id, created_at, deleted_at, updated_at, birthdate, firstname, iin, lastname, living_place, middlename, photo_name, previous_lastname, registration_place, citizenship_id, gender_id, marital_status_id, nationality_id) VALUES (2, '2020-01-28 09:03:00', null, '2020-01-28 09:03:01', '1990-01-28 09:03:04', 'Anna', '900128654789', 'Karenina', 'Kabanbai batyr', 'Andereyevna', 'anna789', 'Sun', 'Abaya123', 2, 2, 2, 2);
+
+INSERT INTO rbasic.roles (id, created_at, deleted_at, updated_at, name) VALUES (1, '2020-01-28 09:05:18', null, '2020-01-28 09:05:22', 'ROLE_USER');
+INSERT INTO rbasic.roles (id, created_at, deleted_at, updated_at, name) VALUES (2, '2020-01-28 09:05:20', null, '2020-01-28 09:05:23', 'ROLE_PM');
+INSERT INTO rbasic.roles (id, created_at, deleted_at, updated_at, name) VALUES (3, '2020-01-28 09:05:21', null, '2020-01-28 09:05:24', 'ROLE_ADMIN');
+
+INSERT INTO rbasic.user_roles (user_id, role_id) VALUES (1, 1);
+INSERT INTO rbasic.user_roles (user_id, role_id) VALUES (1, 2);
+INSERT INTO rbasic.user_roles (user_id, role_id) VALUES (2, 2);
+INSERT INTO rbasic.user_roles (user_id, role_id) VALUES (2, 3);
+
+INSERT INTO rbasic.users_documents (user_id, documents_id) VALUES (1, 1);
+INSERT INTO rbasic.users_documents (user_id, documents_id) VALUES (1, 2);
+INSERT INTO rbasic.users_documents (user_id, documents_id) VALUES (1, 3);
+INSERT INTO rbasic.users_documents (user_id, documents_id) VALUES (2, 4);
+INSERT INTO rbasic.users_documents (user_id, documents_id) VALUES (2, 5);
+
+INSERT INTO rbasic.users_positions (user_id, positions_id) VALUES (1, 1);
+INSERT INTO rbasic.users_positions (user_id, positions_id) VALUES (1, 2);
+INSERT INTO rbasic.users_positions (user_id, positions_id) VALUES (1, 3);
+INSERT INTO rbasic.users_positions (user_id, positions_id) VALUES (2, 4);
+INSERT INTO rbasic.users_positions (user_id, positions_id) VALUES (2, 5);
+
+INSERT INTO rbasic.users (id, created_at, deleted_at, updated_at, password, username, additional_information_id, contacts_id, profile_id) VALUES (1, '2020-01-28 09:05:49', null, '2020-01-28 09:05:52', '$2a$10$5b2PZB.tyXr7uKcHX3d3reWS4zmRX7jNW03aZCLX4.feEdoJJepDq', 'henry', 1, 1, 1);
+INSERT INTO rbasic.users (id, created_at, deleted_at, updated_at, password, username, additional_information_id, contacts_id, profile_id) VALUES (2, '2020-01-28 09:07:30', null, '2020-01-28 09:07:33', '$2a$10$5b2PZB.tyXr7uKcHX3d3reWS4zmRX7jNW03aZCLX4.feEdoJJepDq', 'anna', 2, 2, 2);
+
+
+
